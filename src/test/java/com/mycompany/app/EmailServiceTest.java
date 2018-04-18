2519aeb8085dbe92f988fceb646aead38f46f2cc
Nelson Arape <nelson@spotify.com>
2018-04-18 17:08:56 +0200

    Add tests for EmailService

---
+++ src/test/java/com/mycompany/app/EmailServiceTest.java
@@ -0,0 +1,33 @@
+package com.mycompany.app;
+
+import java.util.Collections;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class EmailServiceTest {
+
+  private static final String BLACKLISTED_ADDRESS = "thieve@evil.com";
+  private static final String EMAIL_BODY = "Please buy milk";
+
+  private final EmailService emailer =
+      new EmailService(Collections.singletonList(BLACKLISTED_ADDRESS));
+
+  @Test
+  public void shouldReturnTrueForAddressesNotInTheBlacklist() {
+    boolean result = emailer.sendEmail("pepe@gmail.com", EMAIL_BODY);
+
+    Assert.assertTrue(result);
+  }
+
+  @Test
+  public void shouldReturnFalseForAddressesInTheBlacklist() {
+    boolean result = emailer.sendEmail(BLACKLISTED_ADDRESS, EMAIL_BODY);
+
+    Assert.assertFalse(result);
+  }
+
+  @Test(expected = NullPointerException.class)
+  public void shouldThrowNullPointerExceptionWhenAddressIsNull() {
+    emailer.sendEmail(null, EMAIL_BODY);
+  }
+}
